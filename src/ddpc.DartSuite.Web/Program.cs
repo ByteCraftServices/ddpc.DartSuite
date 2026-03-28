2c1552c81b83dadc0c495191e333747d25a63242
ByteCraftServices <martin@angleitner.net>
2026-03-28 19:14:11 +0100

    v0.1.1 Dockerfile

--- src/ddpc.DartSuite.Web/Program.cs
+++ src/ddpc.DartSuite.Web/Program.cs
@@ -11,7 +11,7 @@ builder.Services.AddLocalization();
 builder.Services.AddScoped<AppStateService>();
 builder.Services.AddHttpClient<DartSuiteApiService>(client =>
 {
-    client.BaseAddress = new Uri(builder.Configuration["Api:BaseUrl"] ?? "http://localhost:5290/");
+     
 });
 
 var supportedCultures = new[] { "de", "en-US" };
